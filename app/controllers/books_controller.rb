6076b927441db42dd4b6ce4cf383d183269576f6
kameinaoki <nkamegre4nhns9@gmail.com>
2021-09-21 21:44:56 +0000

    20190921

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,23 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @book = Book.new
   end
 
   def edit
@@ -38,7 +39,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end

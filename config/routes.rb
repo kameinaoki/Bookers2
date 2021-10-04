4d9481ab07622332d7e7e38f17b78e6b47636afd
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-04 03:18:18 +0000

    [finish]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   resources :books, only: [ :new, :create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
-    
+    resources :book_comments, only: [:create, :destroy]
   end
   
   resources :users, only: [ :index, :show, :edit, :update]

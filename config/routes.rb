95d65bdcca68a2b00246a5a3e25d99ae5a72f050
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-04 01:18:47 +0000

    [finish]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
@@ -6,7 +7,10 @@ Rails.application.routes.draw do
   
   post "books/:id" => "books#show"
   
-  resources :books, only: [ :new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [ :new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    
+  end
   
   resources :users, only: [ :index, :show, :edit, :update]
   

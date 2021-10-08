12b228564b75778c2f36b79b8d2b20cafa3d52f8
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-08 05:12:32 +0000

    [追加]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
+  
+  get "search" => "searches#search"
+  
+  get "favorites/create"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
